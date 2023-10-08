bcf3936300287c9a0f1a8c8b0c715d39ddf3741c
AMIT <108608256+amitvsuryavanshi04@users.noreply.github.com>
2023-10-08 09:06:08 +0530

    Update code.py

--- code.py
+++ code.py
@@ -11,3 +11,22 @@ ImgGray=np.mean(Img,-1) #this line converts the RGB image to grayscale
 plt.figure()
 plt.imshow(ImgGray,cmap='gray')#to get output of the grayscale image
 plt.axis('off') #as we don't need axis so it's kept off
+
+#so now we will start the compression part using fft2 which is a built in function in the numpy lib
+ImgGrayfft=np.fft.fft2(ImgGray)
+ImgGrayfft_sort=np.sort(np.abs(ImgGrayfft.reshape(-1)))
+
+#we need zero out all the unnecessary fft coefficiencts and retain the required or imp coefficients for compression
+for keep in(0.1,0.05,0.001,0.0002):
+  threshold=ImgGrayfft_sort[int(np.floor((1-keep)*len(ImgGrayfft_sort)))]
+  ind=np.abs(ImgGrayfft)>threshold
+  ImgGrayfft_low=ImgGrayfft*ind
+  Imglow=np.fft.ifft2(ImgGrayfft_low).real
+  plt.figure()
+  plt.imshow(Imglow,cmap='gray')
+  plt.axis('off')
+  plt.title('compressed image:retained:'+str(keep*100)+'%')
+plt.show()
+
+#as you all see below the 10% retained image is much clearer and it has a smaller storage size
+#hence we can conclude that fft can perform the image compression very easily
